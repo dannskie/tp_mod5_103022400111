de06e7eed62bcc72cc0b456c605dc2be659986ec
m.izzan nur fadhan <129825202+dannskie@users.noreply.github.com>
2026-03-29 22:50:18 +0800

    commit message

--- tp_modul5_103022400111/Program.cs
+++ tp_modul5_103022400111/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;   
+    }
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> nim = new DataGeneric<string>("103022400111");
+        nim.PrintData();
+    }
+}
