98cbc31c0be28cfef2e7057fa4344cdaeb406743
m.izzan nur fadhan <129825202+dannskie@users.noreply.github.com>
2026-03-29 22:22:44 +0800

    commit message

--- tp_modul5_103022400111/Program.cs
+++ tp_modul5_103022400111/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<String>("izzan");
+    }
+}
